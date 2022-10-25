f07aba461e3a48d0dad283cfc8dfc0115f9601d6
Hrenaki <shikhovwtr@mail.ru>
2022-10-25 16:30:39 +0300

    Added ControllerExtensions

---
+++ Web/Extensions/ControllerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Extensions
+{
+   public static class ControllerExtensions
+   {
+      private static string controllerStr = nameof(Controller);
+
+      public static string GetName<T>() where T : Controller
+      {
+         string currentControllerFullname = typeof(T).Name;
+         return currentControllerFullname.Substring(0, currentControllerFullname.Length - controllerStr.Length);
+      }
+   }
+}
